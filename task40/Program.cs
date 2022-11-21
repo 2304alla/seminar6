1cefcbb646ca3533867352a0822679a204c66395
allakoval <2448572@gmail.com>
2022-11-21 07:56:36 +0500

    45

---
+++ task40/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Введите число А: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+bool TriangleSide(int a, int b, int c)
+{
+   
+    return a < b + c && b < a + c && c < a + b;
+}
+
+bool res = TriangleSide(num1, num2, num3);
+Console.WriteLine(res ? "Да" : "Нет");
---
+++ task42/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DecToBin(int num)
+{
+   int binNumber=0;
+   int d10=1;
+   while (num>0)
+   {
+    binNumber = binNumber+ num % 2 * d10;
+    num/=2;
+    d10*=10;
+   }
+   return binNumber;
+}
+
+int ReverseNum(int num)
+{
+    int count=0;
+    int res= 0;
+    while (num>0)
+    {
+        count= number%10;
+        res= res*10+count;
+        num/=10;
+    }
+    return res;
+}
+
+string decToBin = DecToBin(number);
+
+Console.WriteLine(decToBin);
+
